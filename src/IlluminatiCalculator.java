3ea25c6cd51bd2e3a640395dd3885c19e8978c79
Karen Garcia <karenkgs@gmail.com>
2017-04-18 22:26:17 -0300

    First step tests improved

--- src/IlluminatiCalculator.java
+++ src/IlluminatiCalculator.java
@@ -3,8 +3,8 @@ public class IlluminatiCalculator {
     public int add(String numbers) {
         if(numbers.isEmpty() || (numbers.replaceAll("\\s", "")).isEmpty()){
             return 0;
-        } else if(numbers.equals("1")) {
-            return 1;
+        } else if(numbers.matches("\\d")) {
+            return Integer.parseInt(numbers); //try-catch
         }
         return 20;
     }
